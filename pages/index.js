d8114a14c806e1f0fb1352923c7539d29f86283e
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-03-29 18:50:00 -0700

    footer change

--- pages/index.js
+++ pages/index.js
@@ -83,8 +83,9 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Strainly
+          <img src="/leaf.svg" alt="leaf" className="logo" />
+          
         </a>
       </footer>
 
