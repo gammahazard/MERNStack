f18e7261a14749df856287c30b1af428ecc0515f
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-03-29 18:35:37 -0700

    initial commit skeleton + example strains

---
+++ pages/controllers/signup.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function ThirdProduct() {
+  return (
+    <>
+      <h1>Sign in page here?</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
---
+++ pages/strains/purple-kush.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function ThirdProduct() {
+  return (
+    <>
+      <h1>purple kush</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
